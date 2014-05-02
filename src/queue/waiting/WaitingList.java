a3e01df72884b9f082e6b5466a11841c42b03e08
B Kleinen <kleinen@infrastructure.de>
2014-05-02 15:02:45 +0200

    simple implementation queue

--- src/queue/waiting/WaitingList.java
+++ src/queue/waiting/WaitingList.java
@@ -2,10 +2,19 @@ package queue.waiting;
 
 public class WaitingList {
 
+	String[] content = new String[5];
+	// zeigt immer auf das nächste freie element
+	// gleich der Anzahl der elemente in der History
+	int counter = 0;
+	int nextInLine = 0;
+
 	public void addToWaitingList(String name) {
+		content[counter++] = name;
 	}
 
 	public String getFirstInLine() {
-		return null;
+		if (counter == 0)
+			return null;
+		return content[nextInLine++];
 	}
 }
--- test/queue/waiting/WaitingListTest.java
+++ test/queue/waiting/WaitingListTest.java
@@ -10,14 +10,25 @@ public class WaitingListTest {
 	public void testOne() {
 		WaitingList waitingList = new WaitingList();
 		waitingList.addToWaitingList("Trick");
-		assertEquals("Trick",waitingList.getFirstInLine());
+		assertEquals("Trick", waitingList.getFirstInLine());
 	}
+
 	@Test
 	public void testThree() {
 		WaitingList waitingList = new WaitingList();
 		waitingList.addToWaitingList("Trick");
 		waitingList.addToWaitingList("Tick");
 		waitingList.addToWaitingList("Track");
-			assertEquals("Trick",waitingList.getFirstInLine());
+		assertEquals("Trick", waitingList.getFirstInLine());
+	}
+
+	@Test
+	public void testThreeTakeOutTwo() {
+		WaitingList waitingList = new WaitingList();
+		waitingList.addToWaitingList("Trick");
+		waitingList.addToWaitingList("Tick");
+		waitingList.addToWaitingList("Track");
+		assertEquals("Trick", waitingList.getFirstInLine());
+		assertEquals("Tick", waitingList.getFirstInLine());
 	}
 }
