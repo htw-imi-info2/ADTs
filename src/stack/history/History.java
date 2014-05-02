0561a34da9f05a42d2395672d0fa80870dfbd5b0
B Kleinen <kleinen@infrastructure.de>
2014-05-02 14:45:33 +0200

    History simple implementation done

--- src/stack/history/History.java
+++ src/stack/history/History.java
@@ -1,10 +1,21 @@
 package stack.history;
 
 public class History {
+
+	String[] content = new String[5];
+	// zeigt immer auf das nächste freie element
+	// gleich der Anzahl der elemente in der History
+	int counter = 0;
+
 	public void addToHistory(String url) {
+		// content[counter++] = url;
+		content[counter] = url;
+		counter = counter + 1;
 	}
 
 	public String getLast() {
-		return null;
+		if (counter == 0)
+			return null;
+		return content[--counter];
 	}
 }
--- test/stack/history/HistoryTest.java
+++ test/stack/history/HistoryTest.java
@@ -1,25 +1,46 @@
 package stack.history;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class HistoryTest {
+	History history;
+
+	@Before
+	public void createHistory() {
+		history = new History();
+	}
+
+	@Test
+	public void testEmpty() {
+		assertEquals(null, history.getLast());
+	}
 
 	@Test
 	public void testHistoryOneElement() {
-		History history = new History();
 		history.addToHistory("http://www.htw-berlin.de");
-		assertEquals("http://www.htw-berlin.de",history.getLast());
+		assertEquals("http://www.htw-berlin.de", history.getLast());
 	}
-	
+
 	@Test
 	public void testHistoryThreeElements() {
-		History history = new History();
 		history.addToHistory("http://www.google");
 		history.addToHistory("http://www.github.com");
 		history.addToHistory("http://www.htw-berlin.de");
-		assertEquals("http://www.htw-berlin.de",history.getLast());
+		assertEquals("http://www.htw-berlin.de", history.getLast());
+	}
+
+	@Test
+	public void testHistoryBackAndForthElements() {
+		history.addToHistory("a");
+		history.addToHistory("b");
+		history.addToHistory("c");
+		assertEquals("c", history.getLast());
+		history.addToHistory("d");
+		assertEquals("d", history.getLast());
+
 	}
 
 }
