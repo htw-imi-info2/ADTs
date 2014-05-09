1c0301b0d649be3c4d01719e92376086576f0f55
B Kleinen <kleinen@infrastructure.de>
2014-05-09 14:56:06 +0200

    started Queue

---
+++ src/queue/Queue.java
@@ -0,0 +1,11 @@
+package queue;
+
+public class Queue {
+	class Node{
+		String data;
+		Node snext;
+	}
+	Node first,last;
+	
+
+}
---
+++ test/queue/QueueTest.java
@@ -0,0 +1,23 @@
+package queue;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class QueueTest {
+/*
+ * isEmpty(new Queue()) 
+   = true  isEmpty(enqueue(q,x)) 
+   = false 
+
+dequeue(new Queue()) = error 
+getFront(new Queue()) = error  
+dequeue(enqueue(q,x)) = 
+   if isEmpty(q) then q     else         enqueue(dequeue(q),x) 
+getFront(enqueue(q,x)) = 
+   if isEmpty(q) then x 
+   else getFront(q)
+ */
+
+
+}
